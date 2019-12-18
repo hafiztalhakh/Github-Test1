a83a654f549f3b419fb514c60c3e902ae6a8e270
Talha619 <hafiz.talhakh@gmail.com>
2019-12-18 15:04:09 +0500

    'yo'

--- Github Test1/Form1.Designer.cs
+++ Github Test1/Form1.Designer.cs
@@ -44,8 +44,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
